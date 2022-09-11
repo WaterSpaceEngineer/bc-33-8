c77968387fc384e53987a4027db655c40fc18931
Vladimir <getskovladimir@gmail.com>
2022-09-11 20:04:15 +0300

    Added images and mobile menu

--- js/modal.js
+++ js/modal.js
@@ -1,17 +1,38 @@
-// const getParametersHeader = document.querySelector('.header').getBoundingClientRect();
-// document.body.style.paddingTop = `${getParametersHeader.height}px`;
-
 (() => {
-    const refs = {
-        openModalBtn: document.querySelector("[data-modal-open]"),
-        closeModalBtn: document.querySelector("[data-modal-close]"),
-        modal: document.querySelector("[data-modal]"),
-    };
 
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
+    const openModalBtn = document.querySelector("[data-modal-open]");
+    const closeModalBtn = document.querySelector("[data-modal-close]");
+    const modal = document.querySelector("[data-modal]");
+    const header = document.querySelector('.header');
+    let scroll = calcScrooll();
 
-    function toggleModal() {
-        refs.modal.classList.toggle("is-hidden");
-    }
+    openModalBtn.addEventListener("click", () => {
+        modal.classList.remove("is-hidden");
+        document.body.style.overflow = 'hidden';
+        document.body.style.marginRight = `${scroll}px`;
+        header.style.right = `${scroll / 2}px`;
+    });
+
+    closeModalBtn.addEventListener("click", () => {
+        modal.classList.add("is-hidden");
+        document.body.style.overflow = '';
+        document.body.style.marginRight = `0px`;
+        header.style.right = `0px`;
+    });
+
+    function calcScrooll () {
+		let div = document.createElement('div');
+
+		div.style.width = '50px';
+		div.style.height = '50px';
+		div.style.overflowY = 'scroll';
+		div.style.visibility = 'hidden';
+
+		document.body.appendChild(div);
+		let scrollWidth = div.offsetWidth - div.clientWidth;
+		div.remove();
+
+		return scrollWidth;
+	}
+
 })();
